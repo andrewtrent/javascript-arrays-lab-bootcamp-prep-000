6ff6d3d4dfbcbe67173dc8b240bb339e16d3a4fd
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-14 19:08:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const app = "I don't do much."
-var kittens = ["Milo","Otis","Garfield"]
+const app = "I don't do much.";
+var kittens = ["Milo","Otis","Garfield"];
 function destructivelyAppendKitten(name){
-  kittens.push(name)
-  return kittens
+  kittens.push(name);
+  return kittens;
 }
 function destructivelyPrependKitten(name){
-  kittens.
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name){
+  
 }
